7988e92475299e7a77b5a8f1449ee5fcb715a7f0
rehmanm <muhammad.abdulrehman@gmail.com>
2023-03-07 21:57:01 -0500

    fixed header

--- app/Header.tsx
+++ app/Header.tsx
@@ -19,7 +19,7 @@ function Header() {
             News
           </h1>
         </Link>
-        <div className="flex items-center justified-end space-x-2">
+        <div className="flex items-center justify-end space-x-2">
         <DarkModeButton />
           <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">
              Subscribe
