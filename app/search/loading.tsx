f360d111dc09342427c6915b7a8b76336d556f09
rehmanm <muhammad.abdulrehman@gmail.com>
2023-03-20 00:46:14 -0400

    adding loader

---
+++ app/search/loading.tsx
@@ -0,0 +1,9 @@
+function loading() {
+  return (
+    <div className="animate-pulse font-serif text-lg text-gray-400 text-center p-10">
+      Loading New Feed
+    </div>
+  );
+}
+
+export default loading;
